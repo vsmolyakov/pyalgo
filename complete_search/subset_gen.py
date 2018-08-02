1032863db5b9867dffa17ac235d5eb2e4e569629
Vadim Smolyakov <vsmolyakov@gmail.com>
2018-08-01 22:12:20 -0400

    subset gen
    subset gen

---
+++ complete_search/subset_gen.py
@@ -0,0 +1,27 @@
+def search(k, n):
+    if (k == n):
+        #process subset
+        print(subset) 
+    else:
+        search(k+1, n)
+        subset.append(k)
+        search(k+1, n)
+        subset.pop()
+    #end if
+
+def bitseq(n):
+    for b in range(1 << n):
+        subset = []
+        for i in range(n):
+            if (b & 1 << i):
+                subset.append(i)
+        #end for
+        print(subset)
+    #end for 
+    
+n = 4
+subset = []
+search(0, n) #recursive
+
+#subset = []
+#bitseq(n)    #iterative
